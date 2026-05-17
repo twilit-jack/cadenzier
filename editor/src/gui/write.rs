092e572f866739d6f1d2ed43de0ac8aca9a48a64
Twilit Jack <twilit.jack@proton.me>
2026-05-17 12:26:53 +0300

    perf(editor/gui/write): use `HashSet` for part visibility
    This also removes the need for a `for` loop in `update`.

--- editor/src/gui/write.rs
+++ editor/src/gui/write.rs
@@ -7,15 +7,19 @@ use iced::{
 	Element, Length,
 	widget::{button, checkbox, column, row, rule, scrollable, text},
 };
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 #[derive(Debug, Default)]
 pub struct Write {
-	pub shown_parts: HashMap<cdnz::PartName, bool>,
+	/// Set of all parts in the active project which are hidden in write viewport.
+	///
+	/// Parts which aren't in this set are shown.
+	pub hidden_parts: HashSet<cdnz::PartName>,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
+	/// If `bool` is `true`, part will be visible. If `bool` is `false`, part will be hidden.
 	TogglePartVisibility(cdnz::PartName, bool),
 	JumpToPart(cdnz::PartName),
 }
@@ -26,35 +30,31 @@ pub enum Action {
 }
 
 impl Write {
-	pub fn update(&mut self, global: &mut GlobalState, message: Message) -> Action {
-		// Make sure all parts are in `self.shown_parts`
-		for name in global.project.parts.keys() {
-			self.shown_parts.entry(name.clone()).or_insert(true);
-		}
+	pub fn update(&mut self, _global: &mut GlobalState, message: Message) -> Action {
 		match message {
-			Message::TogglePartVisibility(name, value) => {
-				let _ = self.shown_parts.insert(name, value);
+			Message::TogglePartVisibility(name, is_visible) => {
+				if is_visible {
+					self.hidden_parts.remove(&name);
+				} else {
+					self.hidden_parts.insert(name);
+				}
 				Action::None
 			}
 			Message::JumpToPart(_name) => todo!(),
 		}
 	}
 
-	pub fn view<'a>(&'a self, _global: &'a GlobalState) -> Element<'a, Message> {
+	pub fn view<'a>(&'a self, global: &'a GlobalState) -> Element<'a, Message> {
 		// Side panel: Shows a list of all parts for toggling which ones are shown, as well as a jump
 		// to a specific part.
 		let mut side_panel = column![text("Parts"), rule::horizontal(2)].spacing(10);
-		for name in self.shown_parts.keys() {
+
+		for name in global.project.parts.keys() {
+			let is_visible = !self.hidden_parts.contains(name);
+
 			side_panel = side_panel.push(row![
-				// Visibility toggle
-				checkbox(
-					*self
-						.shown_parts
-						.get(name)
-						.expect("We're iterating over this")
-				)
-				.on_toggle(|value| Message::TogglePartVisibility(name.clone(), value)),
-				// Label and jump-to button
+				checkbox(is_visible)
+					.on_toggle(|value| Message::TogglePartVisibility(name.clone(), value)),
 				button(name.as_str()).on_press(Message::JumpToPart(name.clone()))
 			]);
 		}
