a1fca595890f52a343c335165e8208504efd1a0e
Twilit Jack <twilit.jack@proton.me>
2026-05-17 12:07:33 +0300

    feat(editor/gui/write): implement side panel

--- editor/src/gui/write.rs
+++ editor/src/gui/write.rs
@@ -5,14 +5,20 @@ use crate::gui::GlobalState;
 
 use iced::{
 	Element, Length,
-	widget::{button, column, row, rule, scrollable, text},
+	widget::{button, checkbox, column, row, rule, scrollable, text},
 };
+use std::collections::HashMap;
 
 #[derive(Debug, Default)]
-pub struct Write {}
+pub struct Write {
+	pub shown_parts: HashMap<cdnz::PartName, bool>,
+}
 
 #[derive(Debug, Clone)]
-pub enum Message {}
+pub enum Message {
+	TogglePartVisibility(cdnz::PartName, bool),
+	JumpToPart(cdnz::PartName),
+}
 
 #[derive(Debug, Clone)]
 pub enum Action {
@@ -20,15 +26,37 @@ pub enum Action {
 }
 
 impl Write {
-	pub fn update(&mut self, _global: &mut GlobalState, _message: Message) -> Action {
-		Action::None
+	pub fn update(&mut self, global: &mut GlobalState, message: Message) -> Action {
+		// Make sure all parts are in `self.shown_parts`
+		for name in global.project.parts.keys() {
+			self.shown_parts.entry(name.clone()).or_insert(true);
+		}
+		match message {
+			Message::TogglePartVisibility(name, value) => {
+				let _ = self.shown_parts.insert(name, value);
+				Action::None
+			}
+			Message::JumpToPart(_name) => todo!(),
+		}
 	}
 
-	pub fn view<'a>(&'a self, global: &'a GlobalState) -> Element<'a, Message> {
-		let mut side_panel = column![text("Layouts"), rule::horizontal(2)].spacing(10);
-
-		for name in global.project.layouts.keys() {
-			side_panel = side_panel.push(button(name.as_str())); // TODO: Add .on_press(...)
+	pub fn view<'a>(&'a self, _global: &'a GlobalState) -> Element<'a, Message> {
+		// Side panel: Shows a list of all parts for toggling which ones are shown, as well as a jump
+		// to a specific part.
+		let mut side_panel = column![text("Parts"), rule::horizontal(2)].spacing(10);
+		for name in self.shown_parts.keys() {
+			side_panel = side_panel.push(row![
+				// Visibility toggle
+				checkbox(
+					*self
+						.shown_parts
+						.get(name)
+						.expect("We're iterating over this")
+				)
+				.on_toggle(|value| Message::TogglePartVisibility(name.clone(), value)),
+				// Label and jump-to button
+				button(name.as_str()).on_press(Message::JumpToPart(name.clone()))
+			]);
 		}
 
 		let viewport = scrollable(
