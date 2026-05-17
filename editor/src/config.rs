268698221cf85f9f0c248a935abe1ab71757db4e
Twilit Jack <twilit.jack@proton.me>
2026-05-17 21:05:48 +0300

    feat(editor): implement config

---
+++ editor/src/config.rs
@@ -0,0 +1,97 @@
+// SPDX-FileCopyrightText: 2026 Twilit Jack <twilit.jack@proton.me>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+use serde::{Deserialize, Serialize};
+use std::{
+	fs::{File, create_dir_all},
+	io::{Error, ErrorKind, Read, Write},
+};
+
+// ======== DEFS ========
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Config {}
+
+// ======== DEFAULT ========
+
+impl Default for Config {
+	fn default() -> Self {
+		Self {}
+	}
+}
+
+// ======== SAVE & LOAD ========
+
+#[derive(Debug, Serialize, Deserialize)]
+struct CborContainer {
+	version: String,
+	config: Config,
+}
+
+const MAGIC_BYTES: &[u8; 4] = b"Conf";
+
+impl Config {
+	pub fn to_bytes(&self) -> Result<Vec<u8>, Error> {
+		let container = CborContainer {
+			version: env!("CARGO_PKG_VERSION").to_string(),
+			config: self.clone(),
+		};
+
+		let mut output = Vec::from(*MAGIC_BYTES);
+		ciborium::ser::into_writer(&container, &mut output)
+			.map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+
+		Ok(output)
+	}
+
+	pub fn from_bytes(data: &[u8]) -> Result<Self, Error> {
+		let reader = std::io::Cursor::new(data);
+		Self::from_reader(reader)
+	}
+
+	pub fn from_reader<R: Read>(mut reader: R) -> Result<Self, Error> {
+		let mut magic = [0u8; 4];
+		reader.read_exact(&mut magic)?;
+
+		if &magic != MAGIC_BYTES {
+			return Err(Error::new(ErrorKind::InvalidData, "Invalid Magic").into());
+		}
+
+		let container: CborContainer = ciborium::de::from_reader(reader)
+			.map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+
+		if container.version != env!("CARGO_PKG_VERSION") {
+			return Err(Error::new(ErrorKind::InvalidData, "Outdated Version").into());
+		}
+
+		Ok(container.config)
+	}
+}
+
+impl Config {
+	pub fn save_to_disk(&self) -> Result<(), Error> {
+		let bytes = self.to_bytes()?;
+
+		let dir = dirs::config_dir()
+			.ok_or_else(|| Error::new(ErrorKind::NotFound, "Config directory not found"))?
+			.join("cadenza");
+
+		create_dir_all(&dir)?;
+
+		let path = dir.join("config.cbor");
+		let mut file = File::create(path)?;
+		file.write_all(&bytes)?;
+
+		Ok(())
+	}
+
+	pub fn load_from_disk() -> Result<Self, Error> {
+		let path = dirs::config_dir()
+			.ok_or_else(|| Error::new(ErrorKind::NotFound, "Config directory not found"))?
+			.join("cadenza")
+			.join("config.cbor");
+
+		let file = File::open(path)?;
+		Self::from_reader(file)
+	}
+}
--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2026 Twilit Jack <twilit.jack@proton.me>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+mod config;
 mod gui;
 
 fn main() -> iced::Result {
