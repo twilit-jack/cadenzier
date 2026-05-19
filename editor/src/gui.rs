120a0a8186d94b6dc550d4ad4f97cad3d4fc40b9
Twilit Jack <twilit.jack@proton.me>
2026-05-19 16:31:55 +0300

    refactor(editor/gui): change `State` and functions to `Editor` with methods

--- editor/src/gui.rs
+++ editor/src/gui.rs
@@ -12,17 +12,17 @@ use setup::Setup;
 use write::Write;
 
 use iced::{
-	keyboard::{self, Event},
 	Element, Subscription, Task,
+	keyboard::{self, Event},
 };
 
 pub(super) fn run() -> iced::Result {
-	iced::application(State::default, update, view)
-		.subscription(subscription)
+	iced::application(Editor::default, Editor::update, Editor::view)
+		.subscription(Editor::subscription)
 		.run()
 }
 
-struct State {
+struct Editor {
 	screen: Screen,
 	global: GlobalState,
 }
@@ -31,7 +31,7 @@ struct GlobalState {
 	project: cdnz::Project,
 }
 
-impl Default for State {
+impl Default for Editor {
 	fn default() -> Self {
 		Self {
 			screen: Screen::Setup(Setup::default()),
@@ -76,91 +76,93 @@ enum Message {
 	Help(help::Message),
 }
 
-fn update(state: &mut State, message: Message) -> Task<Message> {
-	match message {
-		Message::Render(message) => {
-			let Screen::Render(render) = &mut state.screen else {
-				return Task::none();
-			};
-			let action = render.update(&mut state.global, message);
-			match action {
-				render::Action::None => Task::none(),
+impl Editor {
+	fn update(&mut self, message: Message) -> Task<Message> {
+		match message {
+			Message::Render(message) => {
+				let Screen::Render(render) = &mut self.screen else {
+					return Task::none();
+				};
+				let action = render.update(&mut self.global, message);
+				match action {
+					render::Action::None => Task::none(),
+				}
 			}
-		}
-		Message::Setup(message) => {
-			let Screen::Setup(setup) = &mut state.screen else {
-				return Task::none();
-			};
-			let action = setup.update(&mut state.global, message);
-			match action {
-				setup::Action::None => Task::none(),
+			Message::Setup(message) => {
+				let Screen::Setup(setup) = &mut self.screen else {
+					return Task::none();
+				};
+				let action = setup.update(&mut self.global, message);
+				match action {
+					setup::Action::None => Task::none(),
+				}
 			}
-		}
-		Message::Write(message) => {
-			let Screen::Write(write) = &mut state.screen else {
-				return Task::none();
-			};
-			let action = write.update(&mut state.global, message);
-			match action {
-				write::Action::None => Task::none(),
+			Message::Write(message) => {
+				let Screen::Write(write) = &mut self.screen else {
+					return Task::none();
+				};
+				let action = write.update(&mut self.global, message);
+				match action {
+					write::Action::None => Task::none(),
+				}
 			}
-		}
-		Message::Help(message) => {
-			let Screen::Help(help) = &mut state.screen else {
-				return Task::none();
-			};
-			let action = help.update(&mut state.global, message);
-			match action {
-				help::Action::None => Task::none(),
+			Message::Help(message) => {
+				let Screen::Help(help) = &mut self.screen else {
+					return Task::none();
+				};
+				let action = help.update(&mut self.global, message);
+				match action {
+					help::Action::None => Task::none(),
+				}
 			}
 		}
 	}
-}
 
-fn view(state: &State) -> Element<'_, Message> {
-	match &state.screen {
-		Screen::Render(render) => render.view(&state.global).map(Message::Render),
-		Screen::Setup(setup) => setup.view(&state.global).map(Message::Setup),
-		Screen::Write(write) => write.view(&state.global).map(Message::Write),
-		Screen::Help(help) => help.view(&state.global).map(Message::Help),
+	fn view(&self) -> Element<'_, Message> {
+		match &self.screen {
+			Screen::Render(render) => render.view(&self.global).map(Message::Render),
+			Screen::Setup(setup) => setup.view(&self.global).map(Message::Setup),
+			Screen::Write(write) => write.view(&self.global).map(Message::Write),
+			Screen::Help(help) => help.view(&self.global).map(Message::Help),
+		}
+		//let mode_button = |label: &'static str, screen: Screen| {
+		//	button(label)
+		//		.on_press(Message::ScreenSwitch(screen))
+		//		.style(move |theme: &Theme, _| {
+		//			let palette = theme.extended_palette();
+		//			let active = self.screen;
+		//			button::Style {
+		//				background: Some(
+		//					if active {
+		//						palette.primary.base.color
+		//					} else {
+		//						palette.background.base.color
+		//					}
+		//					.into(),
+		//				),
+		//				text_color: palette.background.base.text,
+		//				..button::Style::default()
+		//			}
+		//		})
+		//};
+		//let mode_switch_buttons = row![
+		//	mode_button("Setup", Screen::Setup),
+		//	mode_button("Write", Screen::Write),
+		//	mode_button("Help", Screen::Help),
+		//];
 	}
-	//let mode_button = |label: &'static str, screen: Screen| {
-	//	button(label)
-	//		.on_press(Message::ScreenSwitch(screen))
-	//		.style(move |theme: &Theme, _| {
-	//			let palette = theme.extended_palette();
-	//			let active = state.screen;
-	//			button::Style {
-	//				background: Some(
-	//					if active {
-	//						palette.primary.base.color
-	//					} else {
-	//						palette.background.base.color
-	//					}
-	//					.into(),
-	//				),
-	//				text_color: palette.background.base.text,
-	//				..button::Style::default()
-	//			}
-	//		})
-	//};
-	//let mode_switch_buttons = row![
-	//	mode_button("Setup", Screen::Setup),
-	//	mode_button("Write", Screen::Write),
-	//	mode_button("Help", Screen::Help),
-	//];
-}
 
-fn subscription(state: &State) -> Subscription<Message> {
-	let screen_id = state.screen.get_id();
-	let map_event = move |event| match event {
-		Event::KeyPressed { key, modifiers, .. } => match screen_id {
-			ScreenId::Render => Render::keyboard(key, modifiers).map(Message::Render),
-			ScreenId::Setup => Setup::keyboard(key, modifiers).map(Message::Setup),
-			ScreenId::Write => Write::keyboard(key, modifiers).map(Message::Write),
-			ScreenId::Help => Help::keyboard(key, modifiers).map(Message::Help),
-		},
-		_ => None,
-	};
-	keyboard::listen().filter_map(map_event)
+	fn subscription(&self) -> Subscription<Message> {
+		let screen_id = self.screen.get_id();
+		let map_event = move |event| match event {
+			Event::KeyPressed { key, modifiers, .. } => match screen_id {
+				ScreenId::Render => Render::keyboard(key, modifiers).map(Message::Render),
+				ScreenId::Setup => Setup::keyboard(key, modifiers).map(Message::Setup),
+				ScreenId::Write => Write::keyboard(key, modifiers).map(Message::Write),
+				ScreenId::Help => Help::keyboard(key, modifiers).map(Message::Help),
+			},
+			_ => None,
+		};
+		keyboard::listen().filter_map(map_event)
+	}
 }
